b431e0812531a20e4f852e0e94265e61faa72abb
Matt Rayner <m@rayner.io>
2018-06-24 21:31:32 +0100

    Add JsonClient tests and implementation

--- lambda/test/unit/json_client.spec.ts
+++ lambda/test/unit/json_client.spec.ts
@@ -2,7 +2,8 @@
 
 import 'mocha';
 import { expect } from 'chai';
-import { JsonClient } from '../../src/utils/json_client';
+import { JsonClient, JsonResponseObject } from '../../src/utils/json_client';
+import * as nock from 'nock';
 
 describe('JsonClient', () => {
   describe('constructor', () => {
@@ -20,18 +21,83 @@ describe('JsonClient', () => {
   });
 
   describe('getJson', () => {
-    it('gets some JSON');
+    it('gets some JSON', async () => {
+      nock('https://example.com')
+        .get('/test').reply(200, '{ "test": true }');
 
-    it('follows redirects');
+      let client = new JsonClient('example.com');
+      const result: JsonResponseObject = await client.getJson('/test');
 
-    it('populates the statusCode response correctly');
+      expect(result.json.test).to.eq(true);
+    });
+
+    it('follows redirects', async () => {
+      nock('https://example.com')
+        .get('/test').reply(302, '{ "redirect": false, "test": true }', { "Location": "https://example.com/redirected" });
+
+      nock('https://example.com')
+        .get('/redirected').reply(200, '{ "redirect": true }');
+
+      let client = new JsonClient('example.com');
+      const result: JsonResponseObject = await client.getJson('/test');
+      expect(result.json.redirect).to.eq(true);
+    });
+
+    it('populates the statusCode correctly', async () => {
+      nock('https://example.com')
+        .get('/test').reply(404, '{}');
+
+      let client = new JsonClient('example.com');
+      let result: JsonResponseObject = await client.getJson('/test');
+      expect(result.statusCode).to.eq(404);
+
+      nock('https://example.com')
+        .get('/test2').reply(500, '{}');
+
+      client = new JsonClient('example.com');
+      result = await client.getJson('/test2');
+      expect(result.statusCode).to.eq(500);
+    });
 
     context('https set to true', () => {
-      it('requests https');
+      let client = null;
+
+      beforeEach(() => {
+        nock('https://example.com')
+          .get('/test').reply(200, '{"test": true}');
+
+        client = new JsonClient('example.com', true);
+      });
+
+      afterEach(() => {
+        client = null;
+      });
+
+      it('requests https', async () => {
+        let client = new JsonClient('example.com');
+        const result: JsonResponseObject = await client.getJson('/test');
+        expect(result.json.test).to.eq(true)
+      });
     });
 
     context('https set to false', () => {
-      it('requests http');
+      let client = null;
+
+      beforeEach(() => {
+        nock('http://example.com')
+          .get('/test').reply(200, '{"test": true}');
+
+        client = new JsonClient('example.com', false);
+      });
+
+      afterEach(() => {
+        client = null;
+      });
+
+      it('requests http', async () => {
+        const result: JsonResponseObject = await client.getJson('/test');
+        expect(result.json.test).to.eq(true)
+      });
     });
-  })
+  });
 });
